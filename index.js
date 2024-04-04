27804507af9fb7e01b45bd157c6c46403e89eb40
mcorusa <marinacorusa1@gmail.com>
2024-04-04 17:33:24 +0200

    icon switch

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ accordionBtnEls.forEach(btn=>{
         const answer = btn.nextElementSibling.firstElementChild;
         answer.classList.toggle("hidden");
 
-        if(answer.classList.contains("hidden")){
-            btn.lastElementChild.setAttribute("src", "assets/images/icon-plus.svg");
-        }
-        else{
+
+        answer.classList.contains("hidden")? btn.lastElementChild.setAttribute("src", "assets/images/icon-plus.svg"): btn.lastElementChild.setAttribute("src", "assets/images/icon-minus.svg");
+
+        // if(answer.classList.contains("hidden")){
+        //     btn.lastElementChild.setAttribute("src", "assets/images/icon-plus.svg");
+        // }
+        // else{
             
-            btn.lastElementChild.setAttribute("src", "assets/images/icon-minus.svg");
-        }
+        //     btn.lastElementChild.setAttribute("src", "assets/images/icon-minus.svg");
+        // }
     })
 })
